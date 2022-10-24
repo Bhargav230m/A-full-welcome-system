374f8ac88c9de4accf55f9cfa42a49701d7c7188
Bhargav230m <100185728+Bhargav230m@users.noreply.github.com>
2022-10-24 11:10:55 +0530

    Initial Commit

---
+++ Events/Guild/GuildMemberLeave.js
@@ -0,0 +1,26 @@
+const {EmbedBuilder} = require("discord.js");
+const {GuildMember, Embed, InteractionCollector} = require("discord.js");
+const Schema = require("../../Schemas/leave");
+
+module.exports = {
+    name: "guildMemberLeave",
+    async execute(member) {
+        Schema.findOne({Guild: member.guild.id}, async (err, data) => {
+            if (!data) return;
+            let channel = data.Channel;
+            let Msg = data.Msg || " ";
+
+            const {user, guild} = member;
+            const leaveChannel = member.guild.channels.cache.get(data.Channel);
+
+            const welcomeEmbed = new EmbedBuilder()
+            .setTitle("**Member Left**")
+            .setDescription(data.Msg)
+            .setColor(0x037821)
+            .addFields({name: 'Total members', value: `${guild.memberCount}`})
+            .setTimestamp();
+
+            leaveChannel.send({embeds: [welcomeEmbed]});
+        })
+    }
+}
